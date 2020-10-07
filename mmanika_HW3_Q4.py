d138371bf0697a91e983bac27752ed117c32762b
mumanika <mukul11894@gmail.com>
2020-10-07 17:51:13 -0400

    Changes to code

--- mmanika_HW3_Q4.py
+++ mmanika_HW3_Q4.py
@@ -1,4 +1,6 @@
 import csv
+from matplotlib_venn import venn3
+from matplotlib import pyplot as plt
 
 def main(csv_file):
 
@@ -7,19 +9,32 @@ def main(csv_file):
 
     countries = {}
     bw_dict = {}
+    relay = []
+    guard = []
+    exit = []
     for row in csv_reader:
         if row["Country Code"] in countries.keys():
             countries[row["Country Code"]] += 1
         else:
             countries[row["Country Code"]] = 1
 
-        bw_dict[row["Router Name"]] = float(row["Bandwidth (KB/s)"])
+        bw_dict[row["IP Address"]] = [row["Router Name"], float(row["Bandwidth (KB/s)"])]
 
+        relay.append(row["IP Address"])
+        if row["Flag - Guard"] == "1":
+            guard.append(row["IP Address"])
+        if row["Flag - Exit"] == "1":
+            exit.append(row["IP Address"])
 
     print("The top 5 countries that host the most number of Tor relays are:")
     print(sorted(countries.keys(), key= lambda x: countries[x], reverse=True)[:5])
-    print("The top 5 relay names that contribute bandwidth are:")
-    print(sorted(bw_dict.keys(), key=lambda x: bw_dict[x], reverse=True)[:5])
+    print("The top 5 relays that contribute bandwidth are:")
+    relay_list = sorted(bw_dict.items(), key=lambda x: x[1][1], reverse=True)[:5]
+    for i in relay_list:
+        print("Name: {}\tIP Address: {}\tBandwidth: {}".format(i[1][0], i[0], i[1][1]))
+    venn3([set(relay), set(guard), set(exit)], set_labels=("Relays", "Guards", "Exit Relays"))
+    plt.title("Venn Diagram for Relay types")
+    plt.show()
     f.close()
 
 if __name__ == '__main__':
