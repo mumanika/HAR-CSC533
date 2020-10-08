6bcb99192f28d2fff6338102f16085902d72620e
mumanika <mukul11894@gmail.com>
2020-10-08 01:12:08 -0400

    changed logic for q4

--- mmanika_HW3_Q4.py
+++ mmanika_HW3_Q4.py
@@ -8,10 +8,7 @@ def main(csv_file):
 
     f = open(csv_file, "r")
     csv_reader = csv.DictReader(f)
-    # s = str.encode("Hello")
-    # print(hashlib.md5(s).hexdigest())
     countries = {}
-    bw_dict = {}
     tor_relays = {}
     relay = []
     guard = []
@@ -31,14 +28,27 @@ def main(csv_file):
             "name": row["Router Name"]
         }
     f.close()
+    stats = {
+        "mid_bw": 0,
+        "ex_only_bw": 0,
+        "ex_guard_bw": 0,
+        "guard_only_bw": 0
+    }
     for key, value in tor_relays.items():
-        if value["guard_relay"] == "1":
-            guard.append(key)
-        if value["exit_relay"] == "1":
-            exit.append(key)
+
         if value["guard_relay"] == "0" and value["exit_relay"] == "0":
             relay.append(key)
-
+            stats["mid_bw"] += float(value["bw"])
+        elif value["exit_relay"] == "1" and value["guard_relay"] == "0":
+            exit.append(key)
+            stats["ex_only_bw"] += float(value["bw"])
+        elif value["guard_relay"] == "1" and value["exit_relay"] == "1":
+            exit.append(key)
+            guard.append(key)
+            stats["ex_guard_bw"] += float(value["bw"])
+        else:
+            guard.append(key)
+            stats["guard_only_bw"] += float(value["bw"])
     print("The top 5 countries that host the most number of Tor relays are:")
     print(sorted(countries.keys(), key= lambda x: countries[x], reverse=True)[:5])
     print("The top 5 relays that contribute bandwidth are:")
@@ -48,6 +58,13 @@ def main(csv_file):
     for i in relay_list:
         bw_table.add_row([i["name"], i["hostname"], i["ip"], i["bw"]])
     print(bw_table)
+    print("The bandwidth distribution in the venn groups of relays are as follows:")
+    cum_bw_table = PrettyTable()
+    cum_bw_table.title = "Cumulative Bandwidths Per group"
+    cum_bw_table.field_names = ["Guard Relay Only BW (kbps)", "Middle Relay Only BW (kbps)",
+                                "Exit Relay Only BW (kbps)", "Exit + Guard Relay BW (kbps)"]
+    cum_bw_table.add_row([stats["guard_only_bw"], stats["mid_bw"], stats["ex_only_bw"], stats["ex_guard_bw"]])
+    print(cum_bw_table)
     venn3([set(relay), set(guard), set(exit)], set_labels=("Middle Relays", "Guard Relays", "Exit Relays"))
     plt.title("Venn Diagram for Relay types")
     plt.show()
